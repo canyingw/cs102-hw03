8e878c5e29fe1c81409e33e53b32201d76e2582e
canyingw <canying.wang@cooper.edu>
2019-11-27 23:03:56 -0500

    upload homework 3

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import csv
+from statistics import mean
 
 
 def main():
@@ -8,7 +10,16 @@ def main():
     print(f"Processing input file: {input_file_path}")
 
     # TODO: Fill in the actual logic here!
+    with open(input_file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            newlist = []
+            for numbers in row:
+                newlist.append(float(numbers))
+            mean(newlist)
+            print(f"{mean(newlist)}")
 
 
 if __name__ == "__main__":
     main()
+
